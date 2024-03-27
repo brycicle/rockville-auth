4c1e6cd5f52e9c39c55a8e1c6e2300d97f324b37
philip deyto <philip.deyto326@gmail.com>
2024-03-28 03:52:50 +0800

    feat: add account

---
+++ src/main/java/com/rockville/auth/model/domain/User.java
@@ -0,0 +1,40 @@
+package com.rockville.auth.model.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "role")
+public class User extends AuditingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", unique = true)
+    private String id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "contactNumber", unique = true)
+    private String contactNumber;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "middleName")
+    private String middleName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+}
